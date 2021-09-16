3b5cffa496fe935f30d48674373acf22295527a5
khalish29 <mamanktoti8@gmail.com>
2021-09-16 11:24:51 +0700

    menyambungkan

--- app/src/main/java/learn/idn/kotlinapk/LoginActivity.kt
+++ app/src/main/java/learn/idn/kotlinapk/LoginActivity.kt
@@ -1,14 +1,17 @@
 package learn.idn.kotlinapk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
-import kotlin.math.sign
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var dataEmail: Array<String>
     private lateinit var dataPassword: Array<String>
+    private var progressBar: ProgressBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,7 @@ class LoginActivity : AppCompatActivity() {
         val edtEmail : EditText = findViewById(R.id.edt_email)
         val edtPassword : EditText = findViewById(R.id.edt_password)
         val btnLogin : Button = findViewById(R.id.btn_login)
+        progressBar = findViewById(R.id.progress)
         val btnRegister : Button = findViewById(R.id.btn_register)
         val btnForgetPass : TextView = findViewById(R.id.forgot_password)
         val radioRemember : RadioButton = findViewById(R.id.rememberme)
@@ -25,7 +29,12 @@ class LoginActivity : AppCompatActivity() {
         dataPassword = arrayOf("1234", "4321", "1423")
 
         btnLogin.setOnClickListener{
+            progressBar?.visibility = View.VISIBLE
             signIn(edtEmail.text.toString(), edtPassword.text.toString())
+        }
+
+        btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
 
 
@@ -37,10 +46,14 @@ class LoginActivity : AppCompatActivity() {
             val takeDataEmail = dataEmail.get(i)
             val takeDataPass = dataPassword.get(i)
             if (takeDataEmail == email || takeDataPass == Password){
-                Toast.makeText(this, "LogedIn", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this, "LogedIn", Toast.LENGTH_SHORT).show()
+
+                progressBar?.visibility = View.GONE
+                startActivity(Intent(this, HomeActivity::class.java))
                 break
             }else{
-                Toast.makeText(this, "Akun salah",Toast.LENGTH_SHORT).show()
+                progressBar?.visibility = View.GONE
+                Toast.makeText(this, "Akun salah", Toast.LENGTH_SHORT).show()
             }
         }
     }
