ff4a1ebbe4fafc1880f56cc4d7f90528cf914a2e
khalish29 <mamanktoti8@gmail.com>
2021-10-06 10:49:03 +0700

    menyambungkanss

--- app/src/main/java/learn/idn/kotlinapk/HomeActivity.kt
+++ app/src/main/java/learn/idn/kotlinapk/HomeActivity.kt
@@ -6,11 +6,24 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val datasGambar = arrayOf("https://ds393qgzrxwzn.cloudfront.net/resize/m500x500/cat1/img/images/0/W4XMxSTwLd.jpg",
+                                  "https://ds393qgzrxwzn.cloudfront.net/resize/m500x500/cat1/img/images/0/W4XMxSTwLd.jpg")
+        val datasJudul = arrayOf("baju naruto anak", "baju naruto jepang")
+        val datasBuyer = arrayOf("3000","11000")
+
+        val rvItem : RecyclerView = findViewById(R.id.rv_item)
+        rvItem.adapter = AdapterRecyclerView(this, datasGambar, datasJudul, datasBuyer)
+        rvItem.layoutManager = LinearLayoutManager(this)
+        rvItem.setHasFixedSize(true)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
